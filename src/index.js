0d2f5219aa514353a52a6bbc60dd51e72a1d8dfa
Rebecca <say.maybe@yahoo.com>
2018-03-17 11:14:10 -0500

    tic-tac-toe complete

--- src/index.js
+++ src/index.js
@@ -53,13 +53,15 @@ constructor(props) {
     history: [{
       squares: Array(9).fill(null),
     }],
+    stepNumber: 0,
     xIsNext: true,
   };
 }
+
 handleClick(i) {
-  const history = this.state.history;
+  const history = this.state.history.slice(0, this.state.stepNumber + 1);
   const current = history[history.length -1];
-  const squares = current.state.squares.slice();
+  const squares = current.squares.slice();
   if (calculateWinner(squares) || squares[i]) {
     return;
   }
@@ -68,15 +70,37 @@ handleClick(i) {
     history: history.concat([{
       squares: squares,
     }]),
+    stepNumber: history.length,
     xIsNext: !this.state.xIsNext,
   });
+}
+
+jumpTo(step) {
+  this.setState({
+    stepNumber : step,
+    xISNext: (step % 2) === 0,
+  });
+}
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-    let status;
-    if (winner) {
-      staus = 'Winner: ' + winner;
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li key={move}>
+        <button onClick={() => this.jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+      status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -91,7 +115,7 @@ handleClick(i) {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
